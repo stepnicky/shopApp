3c6049563e14ad5b638223cf9d8276e86f6f0b0e
Adam Stępnicki <adamstepnicki@op.pl>
2022-01-31 18:20:41 +0100

    Add useMemo hook

--- ShopApp/src/components/Product/Product.js
+++ ShopApp/src/components/Product/Product.js
@@ -2,14 +2,14 @@ import styles from './Product.module.scss';
 import ProductImage from '../ProductImage/ProductImage.js';
 import ProductOptions from '../ProductOptions/ProductOptions.js';
 import PropTypes from 'prop-types';
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 
 const Product = props => {
 
   const [currentColor, setCurrentColor] = useState(props.colors[0]);
   const [currentSize, setCurrentSize] = useState(props.sizes[0].name);
   
-  const priceHandler = () => {
+  const priceHandler = useMemo(() => {
     if (currentSize === 'S'){
       return props.basePrice;
     } else if (currentSize === 'M'){
@@ -19,7 +19,7 @@ const Product = props => {
     } else if (currentSize === 'XL'){
         return props.basePrice + props.sizes[3].additionalPrice;  
     }
-  }
+  }, [currentSize, props.sizes, props.basePrice]);
 
   return (
     <article className={styles.product}>
@@ -27,9 +27,9 @@ const Product = props => {
       <div>
         <header>
           <h2 className={styles.name}>{props.title}</h2>
-          <span className={styles.price}>Price: {priceHandler()}$</span>
+          <span className={styles.price}>Price: {priceHandler}$</span>
         </header>
-        <ProductOptions name={props.title} price={priceHandler()} sizes={props.sizes} colors={props.colors} currentColor={currentColor} currentSize={currentSize} setCurrentColor={setCurrentColor} setCurrentSize={setCurrentSize} />
+        <ProductOptions name={props.title} price={priceHandler} sizes={props.sizes} colors={props.colors} currentColor={currentColor} currentSize={currentSize} setCurrentColor={setCurrentColor} setCurrentSize={setCurrentSize} />
       </div>
     </article>
   )
